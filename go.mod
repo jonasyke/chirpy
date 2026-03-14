533004518996a64cb16d56e807b8db8c8cf1cd19
jonasyke <jonasyke171@gmail.com>
2026-03-13 17:06:58 -0800

    stop for the day

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jonasyke/chirpy
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/", fileServer)
+
+	server := &http.Server {
+		Addr: ":8080",
+		Handler: mux,
+	}
+
+	server.ListenAndServe()
+}
+
+
+
+
+
